2eb15faffbd7d78da2b5271ec8eb567bd896d1a5
Animoo <144723877+animoofps@users.noreply.github.com>
2024-06-09 22:36:59 +0200

    anim fixups
    Sped up everything since Alar updated the waitforanim thingies, goes a lot faster and now when the cerberus dies it is INSTANT teleportation

--- DGTokenFarmerGithub.lua
+++ DGTokenFarmerGithub.lua
@@ -9,7 +9,7 @@ local PIN = 1234
 
 local function hasTarget()
     local interacting = API.ReadLpInteracting()
-    if interacting.Id ~= 0 then
+    if interacting.Id ~= 0 and interacting.Life > 0 then
         return true
     else
         return false
@@ -46,16 +46,18 @@ end
 local function enterdung()
     if findNpcOrObject(124285, 15, 12) then
         API.DoAction_Object1(0x39, 0, {124285}, 50)
-        API.RandomSleep2(800, 600, 1000)
-        API.WaitUntilMovingEnds()
-        API.RandomSleep2(500, 300, 500)
+        API.RandomSleep2(400, 200, 400)
+        API.WaitUntilMovingEnds(1, 10)
+        print("Walking stopped")
+        API.RandomSleep2(600, 500, 600)
         if API.Select_Option("Normal mode") then
             print("Normal mode found, continuing")
             API.DoAction_Interface(0xffffffff, 0xffffffff, 0, 1188, 8, -1, 2912)
-        else
-            print("Yes or No found, clicking on No.")
+        elseif API.Select_Option("No") then
+            print("Clicking on No")
             API.DoAction_Interface(0xffffffff, 0xffffffff, 0, 1188, 13, -1, 2912)
             API.RandomSleep2(600, 300, 500)
+            print("Clicking on Normal mode")
             API.DoAction_Interface(0xffffffff, 0xffffffff, 0, 1188, 8, -1, 2912)
         end
         if not findNPC(29296, 15) then
@@ -148,9 +150,9 @@ local function knightcheck1() -- initial walking before the combo 1
             if findNpcOrObject(124355, 5, 12) then
                 API.DoAction_WalkerW(WPOINT.new(newTileX, newTileY, newTileZ))
                 print("Random number: " .. randomNumber)
-                while not API.PInAreaW(SpotCheck, 1) do
+                repeat
                     API.RandomSleep2(100, 10, 10)
-                end
+                until API.PInAreaW(SpotCheck, 1) or not API.Read_LoopyLoop()
             end
         end
     end
@@ -176,13 +178,11 @@ local function knightcheck2() -- initial walking before the combo 2
             local newTileYD = Knight.Tile_XYZ.y + 13
             local newTileZD = Knight.Tile_XYZ.z
             local SpotCheck2 = WPOINT.new(newTileXD, newTileYD, newTileZD)
-            --            API.RandomSleep2(500, 500, 400)
             if API.PInArea(newTileX, 1, newTileY, 1, newTileZ) then
-                --    print("Walking to knight check2")
                 API.DoAction_WalkerW(WPOINT.new(newTileXD, newTileYD, newTileZD))
-                while not API.PInAreaW(SpotCheck2, 1) do
+                repeat
                     API.RandomSleep2(10, 10, 10)
-                end
+                until API.PInAreaW(SpotCheck2, 1) or not API.Read_LoopyLoop()
                 API.RandomSleep2(200, 300, 400)
                 combo()
             end
@@ -198,10 +198,10 @@ local function banking()
     local shouldContinue = true
     API.RandomSleep2(500, 300, 500)
     API.DoAction_NPC(0x33, 1888, {19918}, 50) -- QUICKLOAD
-    API.RandomSleep2(800, 500, 600)
-    API.WaitUntilMovingEnds()
+    API.RandomSleep2(500, 300, 400)
+    API.WaitUntilMovingEnds(1, 5)
     API.DoBankPin(PIN)
-    API.RandomSleep2(1500, 1000, 800) -- sleeping to heal off damage/poison
+    API.RandomSleep2(400, 500, 600) -- sleeping to heal off damage/poison
     if needBank() then
         API.DoAction_NPC(0x33, 1888, {19918}, 50) -- QUICKLOAD
         API.RandomSleep2(800, 500, 600)
@@ -213,59 +213,54 @@ local function banking()
         end
     end
     if shouldContinue then
-        if (API.GetPray_() < 600) then
+        if (API.GetPray_() < 500) then
             print("Prayer at " .. API.GetPray_() .. ", renewing it.")
             API.DoAction_Object1(0x29, 0, {92278}, 50)
             API.RandomSleep2(2000, 1500, 800)
-            API.WaitUntilMovingandAnimEnds()
+            while (API.GetPray_() < 500) do
+                API.RandomSleep2(100, 100, 200)
+            end
         end
     end
 end
 
 local function NPCCheck()
-    local finddog = findNPC(29302, 50)
     local findpyrefiend = findNPC(29272, 50)
-    if not finddog then
-        if findpyrefiend then
+    if findpyrefiend then
+        if not hasTarget() then
             if UTILS.isDeflectMelee() then
                 API.DoAction_Ability("Deflect Melee", 1, API.OFF_ACT_GeneralInterface_route)
                 API.RandomSleep2(500, 300, 200)
             end
             API.DoAction_Ability("Max guild Teleport", 1, API.OFF_ACT_GeneralInterface_route)
-            API.RandomSleep2(1200, 1000, 1500)
-            API.WaitUntilMovingandAnimEnds()
-            API.RandomSleep2(1200, 800, 1000)
-        else
-            local findportal = findNpcOrObject(124361, 17, 0)
-            if findportal then
-                if needBank() then
-                    banking()
-                end
-                if API.Read_LoopyLoop() then
-                    API.DoAction_Object1(0x39, 0, {124361}, 50)
-                    API.RandomSleep2(1200, 1000, 2000)
-                    API.WaitUntilMovingandAnimEnds()
-                else
-                    API.RandomSleep2(200, 200, 200)
-                end
+            API.RandomSleep2(3600, 1300, 1500)
+        end
+    else
+        local findportal = findNpcOrObject(124361, 17, 0)
+        if findportal then
+            if needBank() then
+                banking()
+            end
+            if API.Read_LoopyLoop() then
+                API.DoAction_Object1(0x39, 0, {124361}, 50) -- teleport in max guild
+                API.RandomSleep2(1200, 1000, 2000)
+                API.WaitUntilMovingEnds()
+            else
+                API.RandomSleep2(200, 200, 200)
             end
         end
     end
 end
 
-local function dogwitchfighter2()
-    if findNPC(29302, 5) then
-        if not hasTarget() and not API.CheckAnim(20) then
-            API.DoAction_NPC(0x2a, 1600, {29302}, 50)
-            API.RandomSleep2(800, 600, 400)
-        end
-        if hasTarget() and API.CheckAnim(20) then
-            API.RandomSleep2(400, 600, 500)
-        end
+local function DogFighter()
+    if not hasTarget() and not API.CheckAnim(20) then
+        API.DoAction_NPC(0x2a, 1600, {29302}, 50)
+        API.RandomSleep2(800, 600, 400)
     else
         API.RandomSleep2(200, 200, 300)
-        NPCCheck()
     end
+    API.RandomSleep2(200, 200, 300)
+    NPCCheck()
 end
 
 local function healthCheck()
@@ -280,10 +275,8 @@ local function healthCheck()
     elseif hp < 30 then
         print("Teleporting out")
         API.DoAction_Ability("Max guild Teleport", 1, API.OFF_ACT_GeneralInterface_route)
-        API.RandomSleep2(1200, 1000, 2000)
-        API.WaitUntilMovingandAnimEnds()
-        print("Something funky happened, good luck!")
-        API.Write_LoopyLoop(false)
+        API.RandomSleep2(3600, 1300, 1500)
+        print("Something funky happened..., continuing with the script")
     end
 end
 
@@ -299,9 +292,7 @@ local function deathCheck()
         API.DoAction_Interface(0xffffffff, 0xffffffff, 0, 1626, 72, -1, 2912)
         API.RandomSleep2(1500, 1000, 800)
         API.DoAction_Ability("Max guild Teleport", 1, API.OFF_ACT_GeneralInterface_route)
-        API.RandomSleep2(1200, 1000, 1500)
-        API.WaitUntilMovingandAnimEnds()
-        API.RandomSleep2(1200, 800, 1000)
+        API.RandomSleep2(3600, 1300, 1500)
     end
 end
 
@@ -313,5 +304,5 @@ while API.Read_LoopyLoop() do
     enterdung()
     knightcheck1()
     knightcheck2()
-    dogwitchfighter2()
+    DogFighter()
 end
